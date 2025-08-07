d3437880c09ca3f56bc6ac309f9e76f8d8d7c26c
testgithubritgu <swapnilraut1698@gmail.com>
2025-08-07 11:39:46 +0530

    added lap feature

--- src/Component/Stopwatch.jsx
+++ src/Component/Stopwatch.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from 'react'
 
 const Stopwatch = () => {
+  const [lap,setlap] = useState([])
     const [time,settime] = useState(0)
     let intervalRef = useRef(null)
     let StopwatchRef = useRef(0)
@@ -20,6 +21,11 @@ const Stopwatch = () => {
       clearInterval(intervalRef.current)
       settime(0)
     }
+    function handleLap (){
+      setlap(prev => ([...prev,formateTime()]))
+    
+      console.log(lap)
+    }
     function formateTime (){
       const ms = Math.floor((time%1000)/10).toString().padStart(2,0)
       const s = Math.floor((time/1000)%60).toString().padStart(2,0)
@@ -33,7 +39,16 @@ const Stopwatch = () => {
       <div>
         <button  onClick={handleStart}>start</button>
         <button onClick={handlePause}>pause</button>
-        <button onClick={handleReset}> reset</button>
+        <button onClick={handleReset}>reset</button>
+        <button onClick={handleLap}>Lap</button>
+      </div>
+
+      <div>
+        {lap?.map((e,i)=>(
+          <div>
+            <span>{e}</span>
+          </div>
+        ))}
       </div>
     </>
   )
